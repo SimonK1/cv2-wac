28675e23ea58a9a274b703a44aa45aad44282daf
Simon Kokavec <simikoky@gmail.com>
2025-03-11 13:31:49 +0100

    Added ambulance waiting list API

--- src/components/cv2-ambulance-wl-list/test/cv2-ambulance-wl-list.spec.tsx
+++ src/components/cv2-ambulance-wl-list/test/cv2-ambulance-wl-list.spec.tsx
@@ -1,14 +1,14 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { Cv2AmbulanceWlList } from '../cv2-ambulance-wl-list';
+import { cv2AmbulanceWlList } from '../cv2-ambulance-wl-list';
 
 describe('cv2-ambulance-wl-list', () => {
   it('renders', async () => {
     const page = await newSpecPage({
-      components: [Cv2AmbulanceWlList],
+      components: [cv2AmbulanceWlList],
       html: `<cv2-ambulance-wl-list></cv2-ambulance-wl-list>`,
     });
 
-    const wlList = page.rootInstance as Cv2AmbulanceWlList;
+    const wlList = page.rootInstance as cv2AmbulanceWlList;
     const expectedPatients = wlList?.waitingPatients?.length
 
     const items = page.root.shadowRoot.querySelectorAll("md-list-item");
