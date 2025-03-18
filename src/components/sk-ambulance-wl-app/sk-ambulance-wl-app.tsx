8049568b3f6d042e0b328628317b055d80219bb2
Simon Kokavec <simikoky@gmail.com>
2025-03-18 13:25:17 +0100

    Predpokladaný čas vstupu

--- src/components/sk-ambulance-wl-app/sk-ambulance-wl-app.tsx
+++ src/components/sk-ambulance-wl-app/sk-ambulance-wl-app.tsx
@@ -37,6 +37,7 @@ export class SkAmbulanceWlApp {
   }
 
   render() {
+    console.debug("sk-ambulance-wl-app.render() - path: %s", this.relativePath);
     let element = "list"
     let entryId = "@new"
   
